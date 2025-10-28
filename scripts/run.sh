bdec7e019957fb913bc690cc6c6d30738e05f1f2
HuskyHsu <schi.hsu@gmail.com>
2025-10-28 17:13:09 +0800

    feat: add English / Japan Pokemon data parsing to run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,3 @@
 node scripts/pokemonDataParser.js za-textport/Trad_Chinese/personal.txt data/personal_zh.json
+node scripts/pokemonDataParser.js za-textport/English/personal.txt data/personal_en.json
+node scripts/pokemonDataParser.js za-textport/JPN/personal.txt data/personal_ja.json
