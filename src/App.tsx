86a27d0dc5769055939f0f7b7fe916bfdbf3fec0
HuskyHsu <schi.hsu@gmail.com>
2025-11-25 10:55:59 +0800

    feat: remove About page and route
    Remove unused About page component and its corresponding route to simplify application navigation structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useAnalytics } from '@/hooks/useAnalytics';
 import MainLayout from '@/layouts/MainLayout';
 import { Route, Routes } from 'react-router-dom';
 
-import About from '@/pages/about';
 import Home from '@/pages/home';
 import PokemonDetail from '@/pages/pokemon';
 
@@ -14,7 +13,6 @@ function App() {
     <Routes>
       <Route path='/' element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
         <Route path='pokemon/:link' element={<PokemonDetail />} />
       </Route>
     </Routes>
